9d61ca83f645d078dc472b4e5b2c4521a3d5eb4a
AviPeltz <aj.peltz@gmail.com>
2024-01-16 20:54:38 -0800

    link to annual

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ export default function Page() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center"> 
         <h1 className="text-5xl font-bold">Avi's Calendar <br /> Dashboard</h1>
+        <br />
         <NavLinks />
       </div>
     </main>
