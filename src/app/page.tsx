f903295d1352b7fe390f18f1007718946d4353ab
AviPeltz <aj.peltz@gmail.com>
2024-01-19 08:45:52 -0800

    Add Radix UI dependencies and plant data portal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,16 @@
 import Image from 'next/image'
 import NavLinks from './ui/dashboard/nav-link'
 import SideNav from './ui/dashboard/sidenav';
+import { DataPortal } from '@/components/data-portal';
 
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center"> 
-        <h1 className="text-5xl font-bold">Avi's Calendar <br /> Dashboard</h1>
+        {/*<h1 className="text-5xl font-bold">Avi's Calendar <br /> Dashboard</h1>
         <br />
-        <NavLinks />
+  <NavLinks />*/}
+        <DataPortal />
       </div>
     </main>
   )
